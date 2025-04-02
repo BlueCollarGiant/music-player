28dd8cf41359ea182df3fdfa90beed733e6437ce
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-04-02 10:17:27 -0500

    created player controls component for the footer

---
+++ class-project/src/app/music-player/footer/player-controls/player-controls.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-controls',
+  imports: [],
+  templateUrl: './player-controls.component.html',
+  styleUrl: './player-controls.component.css'
+})
+export class PlayerControlsComponent {
+
+}
