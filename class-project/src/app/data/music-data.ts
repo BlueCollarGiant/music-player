ba0b3712552d072f105e7f9f9692463ea15b4562
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-04-09 17:48:26 -0500

    removed unneeded data in song array, changed playlist header to push user to crud, added @if to the song item placeholder so it only shows up on actual songs

--- class-project/src/app/data/music-data.ts
+++ class-project/src/app/data/music-data.ts
@@ -1,6 +1,4 @@
 import { Song } from "../music-player/Models/song.model";
 
-export const songQue: Song[] =[
-  { id: 1, name: 'Song List', isHeader: true, duration: '0:00'}
-];
+export const songQue: Song[] =[];
 
