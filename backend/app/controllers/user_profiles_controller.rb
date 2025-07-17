20004bdf7ece8c66155a89b411dd5503b27084f2
BlueCollarGiant <Mr.isaiahratliff@gmail.com>
2025-07-17 12:23:55 -0500

    forgot to hit save before last commit im an idiot

--- backend/app/controllers/user_profiles_controller.rb
+++ backend/app/controllers/user_profiles_controller.rb
@@ -1,10 +1,37 @@
 class UserProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :sert_user_profile, only: [:show, :update, :destroy]
+  before_action only: [:show, :update, :destroy] do
+    authorize_self_or_admin!(@user_profile.user_id)
+  end
+  
+  
   def show
+    render json: @user_profile
   end
 
   def update
+    if @user_profile.update(user_profile_params)
+      render json: @user_profile
+    else
+      render json: { errors: @user_profile.errors.full_messages}, status: :unprocessable_entry
+    end
   end
 
   def destroy
+    @user_profile.destroy
+    head :no_content
+  end
+
+  private
+
+  def sert_user_profile
+    @user_profile = UserProfile.find(params[:id])
+  rescue ActiveRecord:: RecordNotFound
+    render json: {errors: ['Profile not found']}, status: :not_found
+  end
+
+  def user_profile_params
+    params.require(:user_profile).permit(:username, :avatar, :spotify_linked, :soundcloud_linked, :youtube_linked )
   end
 end
