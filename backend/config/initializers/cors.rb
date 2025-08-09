ed3bff97644dcdc78fdb65ae50ded852a7fdef88
BlueCollarGiant <Mr.isaiahratliff@gmail.com>
2025-08-08 19:07:50 -0500

    ignore nil FRONTEND_URL in origins

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,8 +1,9 @@
+allowed_origins = ['http://localhost:4200']
+allowed_origins << ENV['FRONTEND_URL'] if ENV['FRONTEND_URL'].present?
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Allow local dev and, if present, your deployed frontend
-    origins 'http://localhost:4200', ENV['FRONTEND_URL']
-
+    origins(*allowed_origins)
     resource '*',
       headers: :any,                                # allow Authorization, Content-Type, etc.
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
